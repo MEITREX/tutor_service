d43d86871a390c9e079a98b8db59ca8656c221af
Can <st173375@stud.uni-stuttgart.de>
2025-07-24 13:55:56 +0200

    Added relevantLinks to the ReponseTypes

--- src/main/java/de/unistuttgart/iste/meitrex/tutor_service/persistence/models/LectureQuestionResponse.java
+++ src/main/java/de/unistuttgart/iste/meitrex/tutor_service/persistence/models/LectureQuestionResponse.java
@@ -3,11 +3,17 @@ package de.unistuttgart.iste.meitrex.tutor_service.persistence.models;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LectureQuestionResponse {
     private String answer;
+    private List<String> links = new ArrayList<>();
 
     public LectureQuestionResponse(String answer) {
         this.answer = answer;
