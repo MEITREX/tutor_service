c1b289462901348854bb128e80a93d4ea43c8e55
Can <st173375@stud.uni-stuttgart.de>
2025-09-04 13:29:08 +0200

    Added answers of questions to prompt via HintGenerationData

--- src/main/java/de/unistuttgart/iste/meitrex/tutor_service/service/HintService.java
+++ src/main/java/de/unistuttgart/iste/meitrex/tutor_service/service/HintService.java
@@ -1,14 +1,12 @@
 package de.unistuttgart.iste.meitrex.tutor_service.service;
 
 import de.unistuttgart.iste.meitrex.common.user_handling.LoggedInUser;
-import de.unistuttgart.iste.meitrex.tutor_service.persistence.models.DocumentRecordSegment;
-import de.unistuttgart.iste.meitrex.tutor_service.persistence.models.HintResponse;
-import de.unistuttgart.iste.meitrex.tutor_service.persistence.models.SemanticSearchResult;
-import de.unistuttgart.iste.meitrex.tutor_service.persistence.models.TemplateArgs;
+import de.unistuttgart.iste.meitrex.tutor_service.persistence.models.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -17,17 +15,26 @@ public class HintService {
     private final OllamaService ollamaService;
     private final SemanticSearchService semanticSearchService;
 
-    private static final List<String> PROMPT_TEMPLATES = List.of(
-            "generate_hint.txt"
+    private static final Map<String, String> PROMPT_TEMPLATES = Map.of(
+            "GENERATION", "generate_hint.md",
+            "QUESTION", "question_prompt_{QUESTION_TYPE}.md"
     );
 
+
     public HintResponse generateHintWithQuestion(
-            String questionText,
+            HintGenerationInput input,
             UUID courseId,
             final LoggedInUser currentUser
     ) {
+        HintGenerationData generationData = getGenerationData(input);
+        String promptName = PROMPT_TEMPLATES.get("QUESTION").replace("{QUESTION_TYPE}", input.getType().toString());
+        List<TemplateArgs> questionPromptArgs = List.of(
+                TemplateArgs.builder().argumentName("questionText").argumentValue(generationData.getQuestionText()).build(),
+                TemplateArgs.builder().argumentName("options").argumentValue(generationData.getOptionsText()).build());
+        String questionPrompt = ollamaService.fillTemplate(promptName, questionPromptArgs);
+
         List<SemanticSearchResult> searchResults =
-                semanticSearchService.semanticSearch(questionText, courseId, currentUser);
+                semanticSearchService.semanticSearch(generationData.getSemanticSearchQuery(), courseId, currentUser);
         if (searchResults.isEmpty()) {
             return new HintResponse("No relevant content found in the lecture for this question");
         }
@@ -42,14 +49,79 @@ public class HintService {
             return new HintResponse("No relevant content found in the lecture for this question");
         }
 
-        String prompt = ollamaService.getTemplate(PROMPT_TEMPLATES.get(0));
-        String contentString = semanticSearchService.formatDocumentSegmentsForPrompt(documentSegments);
+        String prompt = ollamaService.getTemplate(PROMPT_TEMPLATES.get("GENERATION"));
+        String contentString = semanticSearchService.formatIntoNumberedListForPrompt(
+                documentSegments.stream().map(DocumentRecordSegment::getText).toList());
         List<TemplateArgs> promptArgs = List.of(
-                TemplateArgs.builder().argumentName("question").argumentValue(questionText).build(),
+                TemplateArgs.builder().argumentName("questionPrompt").argumentValue(questionPrompt).build(),
                 TemplateArgs.builder().argumentName("content").argumentValue(contentString).build()
         );
 
         return ollamaService.startQuery(HintResponse.class, prompt, promptArgs, new HintResponse("An error occurred"));
+    }
+
+    private HintGenerationData getGenerationData(HintGenerationInput input) {
+        return switch (input.getType()) {
+            case CLOZE -> buildClozeData(input.getCloze());
+            case MULTIPLE_CHOICE -> buildAssociationData(input.getAssociation());
+            case ASSOCIATION -> buildMultipleChoiceData(input.getMultipleChoice());
+            default -> throw new IllegalArgumentException("Unsupported hint question type: " + input.getType());
+        };
+    }
+
+    private HintGenerationData buildMultipleChoiceData(HintGenerationInput.HintMultipleChoiceInput input) {
+        if (input == null || input.getText().isBlank() || input.getAnswers().isEmpty()) {
+            throw new IllegalArgumentException("Multiple choice input is invalid or empty");
+        }
+        String questionText = input.getText();
+        String optionsString = semanticSearchService.formatIntoNumberedListForPrompt(input.getAnswers());
+
+        return HintGenerationData.builder()
+                .questionText(questionText)
+                .optionsText(optionsString)
+                .semanticSearchQuery(questionText)
+                .build();
+    }
+
+    private HintGenerationData buildAssociationData(HintGenerationInput.HintAssociationInput input) {
+        if (input == null || input.getText().isBlank() || input.getPairs().isEmpty()) {
+            throw new IllegalArgumentException("Multiple choice input is invalid or empty");
+        }
+        String questionText = input.getText();
+        String optionsString = semanticSearchService.formatIntoNumberedListForPrompt(
+                input.getPairs().stream().map(pair -> (
+                    "Left: " + pair.getLeft() + " <-> Right: " + pair.getRight()))
+                    .toList());
+
+        return HintGenerationData.builder()
+                .questionText(questionText)
+                .optionsText(optionsString)
+                .semanticSearchQuery(questionText)
+                .build();
+    }
+
+    private HintGenerationData buildClozeData(HintGenerationInput.HintClozeInput input) {
+        if (input == null || input.getText().isBlank() || input.getBlanks().isEmpty()) {
+            throw new IllegalArgumentException("Cloze input is invalid or empty");
+        }
+        String questionText = input.getText();
+        List<String> blanks = input.getBlanks();
+        String optionsString = semanticSearchService.formatIntoNumberedListForPrompt(blanks);
+        String semanticSearchQuery = fillClozeText(questionText, blanks);
+
+        return HintGenerationData.builder()
+                .questionText(questionText)
+                .optionsText(optionsString)
+                .semanticSearchQuery(semanticSearchQuery)
+                .build();
+    }
+
+    private String fillClozeText(String text, List<String> blanks) {
+        String result = text;
+        for (int i = 0; i < blanks.size(); i++) {
+            result = result.replace("[" + (i + 1) + "]", blanks.get(i));
+        }
+        return result;
     }
 
 }
